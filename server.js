d9c99c1326df9c3c9fa476c5f04a3fbc7b69af0e
Logan Stillings <lstillin@u.rochester.edu>
2021-04-05 14:10:24 +0200

    Add files

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+var bp = require("body-parser");
+var path = require("path");
+var session = require("express-session");
+
+app.use(express.static(path.join(__dirname, "./client")));
+app.use(express.static(path.join(__dirname, "./node_modules")));
+app.use(
+  session({
+    secret: "yankeedoodle",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(bp.json());
+
+app.listen(8000, function () {
+  console.log("listening on 8000");
+});
